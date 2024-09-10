4cd92a60fe902868828035ab1fd989b3e27afadd
Kaya Vadhan <kayavadhan@college.harvard.edu>
2024-09-10 17:01:29 -0400

    experiment completed

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -1,1 +1,4 @@
-console.log('Hello World!');
+let courseName = 'Programming for Psychologists';
+console.log(courseName.toUpperCase());
+console.log(courseName.indexOf("Psychologists"));
+console.log(courseName.replace("Psychologists", "Coding"))
