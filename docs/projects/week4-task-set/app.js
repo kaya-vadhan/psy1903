05d7aede60ba99b7a4ffd44363958fa2c9b746d2
Kaya Vadhan <kayavadhan@college.harvard.edu>
2024-09-24 17:11:22 -0400

    converttemp

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -2,3 +2,14 @@ function celciusToFarenheit(celcius) {
     let farenheit = (celcius * 1.8) + 32;
     return farenheit;
 }
+
+function convertTemp(temp, convertTo) {
+    if (convertTo == 'f') {
+        let farenheit = (temp * 1.8) + 32;
+        return farenheit;
+    } else {
+        let celcius = (temp - 32) / 1.8;
+        return celcius;
+    }
+}
+
