dd7378a518cf2fd243936c71db507a0d2ccde343
Kaya Vadhan <kayavadhan@college.harvard.edu>
2024-10-15 17:36:29 -0400

    project edits

--- docs/projects/gender-major-study/experiment.js
+++ docs/projects/gender-major-study/experiment.js
@@ -4,32 +4,156 @@ let timeline = [];
 let welcomeTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-    <h1><span class = 'title'>Welcome to the Math Response Time Task!</span></h1> 
-    <p>In this experiment, you will be shown a series of math questions.</p>
-    <p>Please answer as quickly and accurately as possible.</p>
+    <h1><span class = 'title'>Welcome to the Gender-Major IAT Study!</span></h1> 
+    <p>In this experiment..</p>
     <p>Press <span class='key'>SPACE</span> to begin.</p>
     `,
     choices: [' ']
 };
 
-timeline.push(welcomeTrial);
+//timeline.push(welcomeTrial);
 
 // PRIMER
 let primer = ['stereotypical', 'atypical'];
-
-console.log(initJsPsych().randomization.primer);
+let primerDisplay = primer[Math.floor(Math.random() * primer.length)];
 
 let primerTrial = {
     type: jsPsychSurveyHtmlForm,
     preamble: `
-    <p>INSTRUCTIONS</p>,
-    <img src='images/${initJsPsych().randomization.primer}.png'>
+    <h1><span class = 'title'>Part 1</span></h1> 
+    <p>Consider the following image.</p>,
+    <img src='images/${primerDisplay}.png'>
+    <p>Please describe two things you observe about the image in the text box below.</p>,
     `,
     html: `<p><input type='text' name='answer' id='answer'></p>`,
     autofocus: 'answer', // id of the field we want to auto-focus on when the trial starts
-    button_label: 'Submit Answer',
+    button_label: 'Continue',
+    on_finish: function (data) {
+        data.whichPrime = primerDisplay;
+    }
 }
-timeline.push(primerTrial);
+//timeline.push(primerTrial);
+
+// IAT
+
+let iatInstructions = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+    <h1><span class = 'title'>Part 2</span></h1> 
+    <p>IAT TASK</p>
+    <p>Press <span class='key'>SPACE</span> to begin.</p>
+    `,
+    choices: [' ']
+};
+//timeline.push(iatInstructions);
+
+let iteration = 0
+for (let condition of conditions) {
+    iteration++;
+
+    //INSTRUCTIONS FOR EACH BLOCK
+    let blockInstructions = {
+        type: jsPsychHtmlKeyboardResponse,
+        stimulus: `
+        <h1><span class = 'title'>Part ${iteration} of 4</span></h1> 
+        <p>In this part, the two categories will be: ${condition.categories[0]} and ${condition.categories[1]}</p>
+        <p>Press <span class='key'>SPACE</span> to begin.</p>
+        `,
+        choices: [' ']
+    };
+    timeline.push(blockInstructions);
+    //36 ROUNDS
+    for (i = 0; i < 36; i++) {
+        let wordDisplay = {
+            type: jsPsychHtmlKeyboardResponse,
+            stimulus: `
+            <p>Press <span class='key'>F</span> for ${condition.categories[0]}</p>
+            <p>Press <span class='key'>J</span> for ${condition.categories[1]}</p>
+            <h1><span class = 'target'>${condition.trials[iteration - 1].word[i]}</span></h1>
+            `,
+            choices: ['f', 'j'],
+        };
+        timeline.push(wordDisplay);
+    }
+
+};
+
+
+
+// LIKERT
+
+let likertInstructions = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+    <h1><span class = 'title'>Part 3</span></h1> 
+    <p>Likert Task</p>
+    <p>Press <span class='key'>SPACE</span> to begin.</p>
+    `,
+    choices: [' ']
+};
+timeline.push(likertInstructions)
+
+let labels = [
+    "Strongly Disagree",
+    "Disagree",
+    "Somewhat Disagree",
+    "Neutral",
+    "Somewhat Agree",
+    "Agree",
+    "Strongly Agree"];
+
+let likertTrial = {
+    type: jsPsychSurveyLikert,
+    preamble: 'Rate the following...',
+    randomize_question_order: true,
+    questions: [
+        {
+            prompt: "Poor people today have it easy because they can get government benefits without doing anything good in return.",
+            labels: labels
+        },
+        {
+            prompt: "The government today can’t afford to do much more to help the needy.",
+            labels: labels
+        },
+        {
+            prompt: "Black people who can’t get ahead in this country are mostly responsible for their own condition.",
+            labels: labels
+        },
+        {
+            prompt: "Immigrants today strengthen our country because of their hard work and talents.",
+            labels: labels
+        },
+        {
+            prompt: "Good diplomacy is the best way to ensure peace.",
+            labels: labels
+        },
+        {
+            prompt: "Business corporations make too much profit.",
+            labels: labels
+        },
+        {
+            prompt: "Stricter environmental laws and regulations are worth the cost.",
+            labels: labels
+        },
+        {
+            prompt: "Homosexuality should be accepted by society.",
+            labels: labels
+        },
+    ]
+}
+timeline.push(likertTrial);
+
+
+// DEBRIEF
+let debriefTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+    <h1>Thank you for participating!</h1> 
+    <p>You can close this tab.</p>
+    `,
+    choices: ['NO_KEYS'],
+};
+timeline.push(debriefTrial);
 
 jsPsych.run(timeline);
 
